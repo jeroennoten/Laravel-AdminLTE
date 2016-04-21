0537f22ca8f33555dfda05d41792a7c9b5fd7541
Jeroen Noten <jeroennoten@me.com>
2016-04-22 00:42:20 +0200

    Fix issue with registration link

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -56,15 +56,15 @@
                 </div>
             </form>
             <div class="auth-links">
-                @if (config('adminlte.register_url', 'register'))
-                    <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}"
-                       class="text-center"
-                    >{{ trans('adminlte::adminlte.i_forgot_my_password') }}</a>
-                    <br>
-                @endif
-                <a href="{{ url(config('adminlte.register_url', 'register')) }}"
+                <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}"
                    class="text-center"
-                >{{ trans('adminlte::adminlte.register_a_new_membership') }}</a>
+                >{{ trans('adminlte::adminlte.i_forgot_my_password') }}</a>
+                <br>
+                @if (config('adminlte.register_url', 'register'))
+                    <a href="{{ url(config('adminlte.register_url', 'register')) }}"
+                       class="text-center"
+                    >{{ trans('adminlte::adminlte.register_a_new_membership') }}</a>
+                @endif
             </div>
         </div>
         <!-- /.login-box-body -->
