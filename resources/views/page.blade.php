a7da9d50f6c134b9fe5c55e7a0ddb7a6df36f7d4
Jeroen Noten <jeroennoten@me.com>
2016-05-24 22:33:45 +0200

    Fix hard coded URL and logo in top nav bar

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -21,7 +21,9 @@
             <nav class="navbar navbar-static-top">
                 <div class="container">
                     <div class="navbar-header">
-                        <a href="../../index2.html" class="navbar-brand"><b>Admin</b>LTE</a>
+                        <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" class="navbar-brand">
+                            {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+                        </a>
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
                             <i class="fa fa-bars"></i>
                         </button>
