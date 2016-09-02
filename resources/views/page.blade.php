25532d69b68e0e9dd44c1cb677707cd0b2debcd2
Master Bratac <beau@boutonllc.com>
2016-09-01 21:35:49 -0600

    changed stack directives to js and css

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,7 +3,7 @@
 @section('adminlte_css')
     <link rel="stylesheet"
           href="{{ asset('vendor/adminlte/dist/css/skins/skin-' . config('adminlte.skin', 'blue') . '.min.css')}} ">
-    @stack('css_stack')
+    @stack('css')
     @yield('css')
 @stop
 
@@ -132,6 +132,6 @@
 
 @section('adminlte_js')
     <script src="{{ asset('vendor/adminlte/dist/js/app.min.js') }}"></script>
-    @stack('js_stack')
+    @stack('js')
     @yield('js')
 @stop
