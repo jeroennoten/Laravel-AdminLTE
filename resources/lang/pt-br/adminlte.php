b2d063e9e34b6c05c5e66be3fa3e890719e7ad46
Luiz Toni <luiztoni@outlook.com>
2016-09-09 06:36:35 -0300

    Translations to brazilian portuguese and spanish (#32)

---
+++ resources/lang/pt-br/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nome completo',
+    'email'                       => 'Email',
+    'password'                    => 'Senha',
+    'retype_password'             => 'Repita a senha',
+    'remember_me'                 => 'Lembrar-me',
+    'register'                    => 'Registrar',
+    'register_a_new_membership'   => 'Registrar um novo membro',
+    'i_forgot_my_password'        => 'Esqueci minha senha',
+    'i_already_have_a_membership' => 'Já sou um membro',
+    'sign_in'                     => 'Assinar',
+    'log_out'                     => 'Sair',
+    'toggle_navigation'           => 'Trocar navegação',
+    'login_message'               => 'Entre para iniciar uma nova sessão',
+    'register_message'            => 'Registrar um novo membro',
+    'password_reset_message'      => 'Recuperar senha',
+    'reset_password'              => 'Recuperar senha',
+    'send_password_reset_link'    => 'Enviar link de recuperação de senha',
+];
