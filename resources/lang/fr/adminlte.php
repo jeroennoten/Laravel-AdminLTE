0b3878f181b1e661041d93d7c4a22bb8164852c8
bestmomo <colorstonic@free.fr>
2017-02-02 00:28:35 +0100

    Add french language

---
+++ resources/lang/fr/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nom',
+    'email'                       => 'Email',
+    'password'                    => 'Mot de passe',
+    'retype_password'             => 'Entrez à nouveau le mot de passe',
+    'remember_me'                 => 'Se souvenir de moi',
+    'register'                    => 'Enregistrement',
+    'register_a_new_membership'   => 'Enregistrer un nouveau membre',
+    'i_forgot_my_password'        => 'J\'ai oublié mon mot de passe',
+    'i_already_have_a_membership' => 'J\'ai déjà un compte',
+    'sign_in'                     => 'Connexion',
+    'log_out'                     => 'Déconnexion',
+    'toggle_navigation'           => 'Basculer la navigation',
+    'login_message'               => 'Connectez-vous pour commencer une session',
+    'register_message'            => 'Enregistrement d\'un nouveau membre',
+    'password_reset_message'      => 'Réinitialisation du mot de passe',
+    'reset_password'              => 'Réinitialisation du mot de passe',
+    'send_password_reset_link'    => 'Envoi de la réinitialisation du mot de passe',
+];
