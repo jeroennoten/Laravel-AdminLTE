fa1dfcdf520e2b14fe2cf51417c717494733595b
huaxiachenli <huaxiachenli@163.com>
2017-04-30 19:52:48 +0800

    require csrf taken for adminlte template
    if we use the master.blade.php for our template,we can't use the ajax for my application

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
 @yield('title', config('adminlte.title', 'AdminLTE 2'))
 @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
