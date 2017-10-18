573f5436e591023219e29254af227350de11f525
Hafidz Nasruddin <repo@serkam.my>
2017-10-18 17:33:13 +0800

    Add alerts for model errors validation

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -114,7 +114,23 @@
 
             <!-- Main content -->
             <section class="content">
-
+                
+                @if ($errors->any())
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-warning alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-warning"></i> Alert!</h4>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                
                 @yield('content')
 
             </section>
