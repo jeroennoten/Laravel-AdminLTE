1a7582335735ad62d06a511c2459fdc0905d2ea6
Jeroen Noten <jeroennoten@me.com>
2018-09-20 22:47:18 +0200

    Fix incompatibility with Laravel 5.7

--- resources/views/partials/menu-item.blade.php
+++ resources/views/partials/menu-item.blade.php
@@ -5,11 +5,11 @@
         <a href="{{ $item['href'] }}"
            @if (isset($item['target'])) target="{{ $item['target'] }}" @endif
         >
-            <i class="fa fa-fw fa-{{ $item['icon'] or 'circle-o' }} {{ isset($item['icon_color']) ? 'text-' . $item['icon_color'] : '' }}"></i>
+            <i class="fa fa-fw fa-{{ isset($item['icon']) ? $item['icon'] : 'circle-o' }} {{ isset($item['icon_color']) ? 'text-' . $item['icon_color'] : '' }}"></i>
             <span>{{ $item['text'] }}</span>
             @if (isset($item['label']))
                 <span class="pull-right-container">
-                    <span class="label label-{{ $item['label_color'] or 'primary' }} pull-right">{{ $item['label'] }}</span>
+                    <span class="label label-{{ isset($item['label_color']) ? $item['label_color'] : 'primary' }} pull-right">{{ $item['label'] }}</span>
                 </span>
             @elseif (isset($item['submenu']))
                 <span class="pull-right-container">
