6497f486f7c243144b603d88c64b729fccab7838
Roberto Gallea <roberto.gallea@unipa.it>
2019-02-28 12:45:44 +0100

    Fix compatibility with laravel 5.8

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -18,7 +18,7 @@ class ServiceProviderTest extends TestCase
 
         ServiceProvider::registerMenu($events, $config);
 
-        if (method_exists($events,'dispatch')) {
+        if (method_exists($events, 'dispatch')) {
             $events->dispatch(new BuildingMenu($menuBuilder));
         } else {
             $events->fire(new BuildingMenu($menuBuilder));
