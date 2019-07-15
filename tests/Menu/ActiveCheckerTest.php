69587d125a710135a42eea01d31755ad72a976ab
Florian Ressel <florian@genxtreme.de>
2019-07-15 19:06:11 +0200

    add regex test case

--- tests/Menu/ActiveCheckerTest.php
+++ tests/Menu/ActiveCheckerTest.php
@@ -140,4 +140,11 @@ class ActiveCheckerTest extends TestCase
 
         $this->assertTrue($checker->isActive(['url' => 'menu/item1']));
     }
+
+	public function testRegex()
+    {
+		$checker = $this->makeActiveChecker('http://example.com/post/1');
+
+		$this->assertTrue($checker->isActive(['active' => 'post/[0-9]']));
+	}
 }
