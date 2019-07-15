3866a1922b1a08953ecc14536ac6157235c6908d
Florian Ressel <info@k-30.de>
2019-07-15 20:04:06 +0200

    rename test case function for regex

--- tests/Menu/ActiveCheckerTest.php
+++ tests/Menu/ActiveCheckerTest.php
@@ -141,7 +141,7 @@ class ActiveCheckerTest extends TestCase
         $this->assertTrue($checker->isActive(['url' => 'menu/item1']));
     }
     
-    public function testRegex()
+    public function testExplicitActiveRegex()
     {
         $checker = $this->makeActiveChecker('http://example.com/posts/1');
 
