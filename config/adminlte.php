eb5f1dd7192c823d83b4694db83076048e0a07de
Anderson Carpi <kustelac@yahoo.com.br>
2019-07-25 17:30:11 -0300

    Identation Fix * Don't know how, but i broke config file identation. Fixed

--- config/adminlte.php
+++ config/adminlte.php
@@ -2,283 +2,283 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Title
-	|--------------------------------------------------------------------------
-	|
-	| The default title of your admin panel, this goes into the title tag
-	| of your page. You can override it per page with the title section.
-	| You can optionally also specify a title prefix and/or postfix.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Title
+    |--------------------------------------------------------------------------
+    |
+    | The default title of your admin panel, this goes into the title tag
+    | of your page. You can override it per page with the title section.
+    | You can optionally also specify a title prefix and/or postfix.
+    |
+    */
 
-	'title' => 'AdminLTE 2',
+    'title' => 'AdminLTE 2',
 
-	'title_prefix' => '',
+    'title_prefix' => '',
 
-	'title_postfix' => '',
+    'title_postfix' => '',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Logo
-	|--------------------------------------------------------------------------
-	|
-	| This logo is displayed at the upper left corner of your admin panel.
-	| You can use basic HTML here if you want. The logo has also a mini
-	| variant, used for the mini side bar. Make it 3 letters or so
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Logo
+    |--------------------------------------------------------------------------
+    |
+    | This logo is displayed at the upper left corner of your admin panel.
+    | You can use basic HTML here if you want. The logo has also a mini
+    | variant, used for the mini side bar. Make it 3 letters or so
+    |
+    */
 
-	'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Admin</b>LTE',
 
-	'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>A</b>LT',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Skin Color
-	|--------------------------------------------------------------------------
-	|
-	| Choose a skin color for your admin panel. The available skin colors:
-	| blue, black, purple, yellow, red, and green. Each skin also has a
-	| light variant: blue-light, purple-light, purple-light, etc.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Skin Color
+    |--------------------------------------------------------------------------
+    |
+    | Choose a skin color for your admin panel. The available skin colors:
+    | blue, black, purple, yellow, red, and green. Each skin also has a
+    | light variant: blue-light, purple-light, purple-light, etc.
+    |
+    */
 
-	'skin' => 'blue',
+    'skin' => 'blue',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Layout
-	|--------------------------------------------------------------------------
-	|
-	| Choose a layout for your admin panel. The available layout options:
-	| null, 'boxed', 'fixed', 'top-nav'. null is the default, top-nav
-	| removes the sidebar and places your menu in the top navbar
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Layout
+    |--------------------------------------------------------------------------
+    |
+    | Choose a layout for your admin panel. The available layout options:
+    | null, 'boxed', 'fixed', 'top-nav'. null is the default, top-nav
+    | removes the sidebar and places your menu in the top navbar
+    |
+    */
 
-	'layout' => null,
+    'layout' => null,
 
-	/*
-	|--------------------------------------------------------------------------
-	| Collapse Sidebar
-	|--------------------------------------------------------------------------
-	|
-	| Here we choose and option to be able to start with a collapsed side
-	| bar. To adjust your sidebar layout simply set this  either true
-	| this is compatible with layouts except top-nav layout option
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Collapse Sidebar
+    |--------------------------------------------------------------------------
+    |
+    | Here we choose and option to be able to start with a collapsed side
+    | bar. To adjust your sidebar layout simply set this  either true
+    | this is compatible with layouts except top-nav layout option
+    |
+    */
 
-	'collapse_sidebar' => false,
+    'collapse_sidebar' => false,
 
-	/*
-	|--------------------------------------------------------------------------
-	| URLs
-	|--------------------------------------------------------------------------
-	|
-	| Register here your dashboard, logout, login and register URLs. The
-	| logout URL automatically sends a POST request in Laravel 5.3 or higher.
-	| You can set the request to a GET or POST with logout_method.
-	| Set register_url to null if you don't want a register link.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | URLs
+    |--------------------------------------------------------------------------
+    |
+    | Register here your dashboard, logout, login and register URLs. The
+    | logout URL automatically sends a POST request in Laravel 5.3 or higher.
+    | You can set the request to a GET or POST with logout_method.
+    | Set register_url to null if you don't want a register link.
+    |
+    */
 
-	'dashboard_url' => 'home',
+    'dashboard_url' => 'home',
 
-	'logout_url' => 'logout',
+    'logout_url' => 'logout',
 
-	'logout_method' => null,
+    'logout_method' => null,
 
-	'login_url' => 'login',
+    'login_url' => 'login',
 
-	'register_url' => 'register',
+    'register_url' => 'register',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Menu Items
-	|--------------------------------------------------------------------------
-	|
-	| Specify your menu items to display in the left sidebar. Each menu item
-	| should have a text and a URL. You can also specify an icon from Font
-	| Awesome. A string instead of an array represents a header in sidebar
-	| layout. The 'can' is a filter on Laravel's built in Gate functionality.
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Items
+    |--------------------------------------------------------------------------
+    |
+    | Specify your menu items to display in the left sidebar. Each menu item
+    | should have a text and a URL. You can also specify an icon from Font
+    | Awesome. A string instead of an array represents a header in sidebar
+    | layout. The 'can' is a filter on Laravel's built in Gate functionality.
+    */
 
-	'menu' => [
-		[
-			'text' => 'search',
-			'search' => true,
-		],
-		['header' => 'main_navigation'],
-		[
-			'text' => 'blog',
-			'url'  => 'admin/blog',
-			'can'  => 'manage-blog',
-		],
-		[
-			'text'        => 'pages',
-			'url'         => 'admin/pages',
-			'icon'        => 'far fa-file',
-			'label'       => 4,
-			'label_color' => 'success',
-		],
-		['header' => 'account_settings'],
-		[
-			'text' => 'profile',
-			'url'  => 'admin/settings',
-			'icon' => 'fas fa-fw fa-user',
-		],
-		[
-			'text' => 'change_password',
-			'url'  => 'admin/settings',
-			'icon' => 'fas fa-fw fa-lock',
-		],
-		[
-			'text'    => 'multilevel',
-			'icon'    => 'fas fa-fw fa-share',
-			'submenu' => [
-				[
-					'text' => 'level_one',
-					'url'  => '#',
-				],
-				[
-					'text'    => 'level_one',
-					'url'     => '#',
-					'submenu' => [
-						[
-							'text' => 'level_two',
-							'url'  => '#',
-						],
-						[
-							'text'    => 'level_two',
-							'url'     => '#',
-							'submenu' => [
-								[
-									'text' => 'level_three',
-									'url'  => '#',
-								],
-								[
-									'text' => 'level_three',
-									'url'  => '#',
-								],
-							],
-						],
-					],
-				],
-				[
-					'text' => 'level_one',
-					'url'  => '#',
-				],
-			],
-		],
-		['header' => 'labels'],
-		[
-			'text'       => 'important',
-			'icon_color' => 'red',
-		],
-		[
-			'text'       => 'warning',
-			'icon_color' => 'yellow',
-		],
-		[
-			'text'       => 'information',
-			'icon_color' => 'aqua',
-		],
-	],
+    'menu' => [
+        [
+            'text' => 'search',
+            'search' => true,
+        ],
+        ['header' => 'main_navigation'],
+        [
+            'text' => 'blog',
+            'url'  => 'admin/blog',
+            'can'  => 'manage-blog',
+        ],
+        [
+            'text'        => 'pages',
+            'url'         => 'admin/pages',
+            'icon'        => 'far fa-file',
+            'label'       => 4,
+            'label_color' => 'success',
+        ],
+        ['header' => 'account_settings'],
+        [
+            'text' => 'profile',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-user',
+        ],
+        [
+            'text' => 'change_password',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-lock',
+        ],
+        [
+            'text'    => 'multilevel',
+            'icon'    => 'fas fa-fw fa-share',
+            'submenu' => [
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ],
+                [
+                    'text'    => 'level_one',
+                    'url'     => '#',
+                    'submenu' => [
+                        [
+                            'text' => 'level_two',
+                            'url'  => '#',
+                        ],
+                        [
+                            'text'    => 'level_two',
+                            'url'     => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'level_three',
+                                    'url'  => '#',
+                                ],
+                                [
+                                    'text' => 'level_three',
+                                    'url'  => '#',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ],
+            ],
+        ],
+        ['header' => 'labels'],
+        [
+            'text'       => 'important',
+            'icon_color' => 'red',
+        ],
+        [
+            'text'       => 'warning',
+            'icon_color' => 'yellow',
+        ],
+        [
+            'text'       => 'information',
+            'icon_color' => 'aqua',
+        ],
+    ],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Menu Filters
-	|--------------------------------------------------------------------------
-	|
-	| Choose what filters you want to include for rendering the menu.
-	| You can add your own filters to this array after you've created them.
-	| You can comment out the GateFilter if you don't want to use Laravel's
-	| built in Gate functionality
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Menu Filters
+    |--------------------------------------------------------------------------
+    |
+    | Choose what filters you want to include for rendering the menu.
+    | You can add your own filters to this array after you've created them.
+    | You can comment out the GateFilter if you don't want to use Laravel's
+    | built in Gate functionality
+    |
+    */
 
-	'filters' => [
-		JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\SubmenuFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-	],
+    'filters' => [
+        JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\SubmenuFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
+    ],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Plugins Initialization
-	|--------------------------------------------------------------------------
-	|
-	| Configure which JavaScript plugins should be included. At this moment,
-	| DataTables, Select2, Chartjs and SweetAlert are added out-of-the-box,
-	| including the Javascript and CSS files from a CDN via script and link tag.
-	| Plugin Name, active status and files array (even empty) are required.
-	| Files, when added, need to have type (js or css), asset (true or false) and location (string).
-	| When asset is set to true, the location will be output using asset() function.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Plugins Initialization
+    |--------------------------------------------------------------------------
+    |
+    | Configure which JavaScript plugins should be included. At this moment,
+    | DataTables, Select2, Chartjs and SweetAlert are added out-of-the-box,
+    | including the Javascript and CSS files from a CDN via script and link tag.
+    | Plugin Name, active status and files array (even empty) are required.
+    | Files, when added, need to have type (js or css), asset (true or false) and location (string).
+    | When asset is set to true, the location will be output using asset() function.
+    |
+    */
 
-	'plugins' => [
-		[
-			'name' => 'Datatables',
-			'active' => true,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js',
-				],
-				[
-					'type' => 'css',
-					'asset' => false,
-					'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css',
-				],
-			],
-		],
-		[
-			'name' => 'Select2',
-			'active' => true,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
-				],
-				[
-					'type' => 'css',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
-				],
-			],
-		],
-		[
-			'name' => 'Chartjs',
-			'active' => true,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
-				],
-			],
-		],
-		[
-			'name' => 'Sweetalert2',
-			'active' => true,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//unpkg.com/sweetalert/dist/sweetalert.min.js',
-				],
-			],
-		],
+    'plugins' => [
+        [
+            'name' => 'Datatables',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css',
+                ],
+            ],
+        ],
+        [
+            'name' => 'Select2',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
+                ],
+            ],
+        ],
+        [
+            'name' => 'Chartjs',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
+                ],
+            ],
+        ],
+        [
+            'name' => 'Sweetalert2',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//unpkg.com/sweetalert/dist/sweetalert.min.js',
+                ],
+            ],
+        ],
         [
             'name' => 'Pace',
             'active' => true,
@@ -295,5 +295,5 @@ return [
                 ],
             ],
         ],
-	],
+    ],
 ];
