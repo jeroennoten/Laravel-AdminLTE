c13fc72d455755b5ac69e5bb0eb5ff682fb7ca16
Daniyal Javani <daniyal.javani@gmail.com>
2019-07-26 15:45:25 +0430

    Add Farsi translate (#331)

---
+++ resources/lang/fa/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'نام',
+    'email'                       => 'ایمیل',
+    'password'                    => 'رمز عبور',
+    'retype_password'             => 'تکرار رمز عبور',
+    'remember_me'                 => 'مرا به یاد داشته باش',
+    'register'                    => 'ثبت نام',
+    'register_a_new_membership'   => 'ایجاد یک عضویت جدید',
+    'i_forgot_my_password'        => 'رمز عبور را فراموش کرده ام',
+    'i_already_have_a_membership' => 'قبلا ثبت نام کرده ام',
+    'sign_in'                     => 'ورود',
+    'log_out'                     => 'خروج',
+    'toggle_navigation'           => 'نمایش/مخفی کردن منو',
+    'login_message'               => 'وارد شوید',
+    'register_message'            => 'ثبت نام',
+    'password_reset_message'      => 'بازنشانی رمز عبور',
+    'reset_password'              => 'بازنشانی رمز عبور',
+    'send_password_reset_link'    => 'ارسال لینک بازنشانی رمز عبور',
+];
