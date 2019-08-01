b960f855f9088e1c6c1602a1541f63735be7f64e
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2019-08-02 01:52:12 +0300

    Turkish translations added

---
+++ resources/lang/tr/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Ad ve Soyadı',
+    'email'                       => 'E-Posta Adresi',
+    'password'                    => 'Parola',
+    'retype_password'             => 'Yeniden Parola',
+    'remember_me'                 => 'Beni Hatırla',
+    'register'                    => 'Kaydol',
+    'register_a_new_membership'   => 'Yeni üye kaydı',
+    'i_forgot_my_password'        => 'Parolamı unuttum',
+    'i_already_have_a_membership' => 'Zaten üye kaydım var',
+    'sign_in'                     => 'Giriş Yap',
+    'log_out'                     => 'Çıkış Yap',
+    'toggle_navigation'           => 'Ana menüyü aç/kapa',
+    'login_message'               => 'Oturumunuzu devam ettirmek için giriş yapmalısınız',
+    'register_message'            => 'Yeni üye kaydı oluştur',
+    'password_reset_message'      => 'Parola Sıfırlama',
+    'reset_password'              => 'Parola Sıfırlama',
+    'send_password_reset_link'    => 'Parola Sıfırlama Linki Gönder',
+];
