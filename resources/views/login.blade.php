7af519c6bd84d6e21d3b71f9fd7ae918eaca0ecf
Anderson Carpi <andcarpi@users.noreply.github.com>
2019-08-26 05:51:43 -0300

    iCheck "upgrade" to icheck-bootstrap (#343)
    * Replaced iCheck for iCheck-Bootstrap in login.blade.php
    (make it default plugin?)
    * Few changes on login, register, email and reset blades.php to eliminate auth.css file and better
    code readability.
    
    Little changes to Readme
    * Address the new breaking changes (Plugins) and if approved (iCheck replace)
    * Info about how to add an Input field in the menu
    * Requirements as a list
    * 5.2.1 to 6.2.1 in Pace Plugin Configuration

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,7 @@
 @extends('adminlte::master')
 
 @section('adminlte_css')
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     @yield('css')
 @stop
 
@@ -17,7 +16,7 @@
         <div class="login-box-body">
             <p class="login-box-msg">{{ trans('adminlte::adminlte.login_message') }}</p>
             <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
-                {!! csrf_field() !!}
+                {{ csrf_field() }}
 
                 <div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
                     <input type="email" name="email" class="form-control" value="{{ old('email') }}"
@@ -41,46 +40,38 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-8">
-                        <div class="checkbox icheck">
-                            <label>
-                                <input type="checkbox" name="remember"> {{ trans('adminlte::adminlte.remember_me') }}
-                            </label>
+                        <div class="icheck-primary">
+                            <input type="checkbox" name="remember" id="remember">
+                            <label for="remember">{{ trans('adminlte::adminlte.remember_me') }}</label>
                         </div>
                     </div>
                     <!-- /.col -->
                     <div class="col-xs-4">
-                        <button type="submit"
-                                class="btn btn-primary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">
+                            {{ trans('adminlte::adminlte.sign_in') }}
+                        </button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
-            <div class="auth-links">
-                <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}"
-                   class="text-center"
-                >{{ trans('adminlte::adminlte.i_forgot_my_password') }}</a>
-                <br>
-                @if (config('adminlte.register_url', 'register'))
-                    <a href="{{ url(config('adminlte.register_url', 'register')) }}"
-                       class="text-center"
-                    >{{ trans('adminlte::adminlte.register_a_new_membership') }}</a>
-                @endif
-            </div>
+            <br>
+            <p>
+                <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}" class="text-center">
+                    {{ trans('adminlte::adminlte.i_forgot_my_password') }}
+                </a>
+            </p>
+            @if (config('adminlte.register_url', 'register'))
+                <p>
+                    <a href="{{ url(config('adminlte.register_url', 'register')) }}" class="text-center">
+                        {{ trans('adminlte::adminlte.register_a_new_membership') }}
+                    </a>
+                </p>
+            @endif
         </div>
         <!-- /.login-box-body -->
     </div><!-- /.login-box -->
 @stop
 
 @section('adminlte_js')
-    <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
-    <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%' // optional
-            });
-        });
-    </script>
     @yield('js')
 @stop
