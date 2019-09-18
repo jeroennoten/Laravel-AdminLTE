1ef6b4bbda69ffa926c42b73790cda33c543e6e7
REJack <info@rejack.de>
2019-09-18 14:02:45 +0200

    removed skin load from page.blade.php

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,8 +1,6 @@
 @extends('adminlte::master')
 
 @section('adminlte_css')
-    <link rel="stylesheet"
-          href="{{ asset('vendor/adminlte/dist/css/skins/skin-' . config('adminlte.skin', 'blue') . '.min.css')}} ">
     @stack('css')
     @yield('css')
 @stop
