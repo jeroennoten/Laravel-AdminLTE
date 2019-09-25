43176d65d0223700b4ddbaa88f2dbabc9e4b510b
Florian Ressel <florian@genxtreme.de>
2019-09-25 09:53:10 +0200

    merge service provider

---
+++ src/AdminLteServiceProvider.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace JeroenNoten\LaravelAdminLte;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Container\Container;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use JeroenNoten\LaravelAdminLte\Console\AdminLteInstallCommand;
+use JeroenNoten\LaravelAdminLte\Http\ViewComposers\AdminLteComposer;
+
+class AdminLteServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(AdminLte::class, function (Container $app) {
+            return new AdminLte(
+                $app['config']['adminlte.filters'],
+                $app['events'],
+                $app
+            );
+        });
+    }
+
+    public function boot(
+        Factory $view,
+        Dispatcher $events,
+        Repository $config
+    ) {
+        $this->loadViews();
+
+        $this->loadTranslations();
+
+        $this->publishConfig();
+
+        $this->publishAssets();
+
+        $this->registerCommands();
+
+        $this->registerViewComposers($view);
+
+        static::registerMenu($events, $config);
+    }
+
+    private function loadViews()
+    {
+        $viewsPath = $this->packagePath('resources/views');
+
+        $this->loadViewsFrom($viewsPath, 'adminlte');
+
+        $this->publishes([
+            $viewsPath => base_path('resources/views/vendor/adminlte'),
+        ], 'views');
+    }
+
+    private function loadTranslations()
+    {
+        $translationsPath = $this->packagePath('resources/lang');
+
+        $this->loadTranslationsFrom($translationsPath, 'adminlte');
+
+        $this->publishes([
+            $translationsPath => base_path('resources/lang/vendor/adminlte'),
+        ], 'translations');
+    }
+
+    private function publishConfig()
+    {
+        $configPath = $this->packagePath('config/adminlte.php');
+
+        $this->publishes([
+            $configPath => config_path('adminlte.php'),
+        ], 'config');
+
+        $this->mergeConfigFrom($configPath, 'adminlte');
+    }
+
+    private function publishAssets()
+    {
+        $this->publishes([
+            $this->packagePath('resources/assets') => public_path(),
+        ], 'assets');
+    }
+
+    private function packagePath($path)
+    {
+        return __DIR__."/../$path";
+    }
+
+    private function registerCommands()
+    {
+        $this->commands(AdminLteInstallCommand::class);
+    }
+
+    private function registerViewComposers(Factory $view)
+    {
+        $view->composer('adminlte::page', AdminLteComposer::class);
+    }
+
+    public static function registerMenu(Dispatcher $events, Repository $config)
+    {
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) use ($config) {
+            $menu = $config->get('adminlte.menu');
+            call_user_func_array([$event->menu, 'add'], $menu);
+        });
+    }
+}
