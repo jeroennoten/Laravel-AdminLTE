f01dfc374375502b5805881bd5b1511be441ff9d
REJack <info@rejack.de>
2019-10-08 11:49:29 +0200

    added adminlte:update command

---
+++ src/Console/AdminLteUpdateCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace JeroenNoten\LaravelAdminLte\Console;
+
+use Illuminate\Console\Command;
+use JeroenNoten\LaravelAdminLte\Http\Helpers\CommandHelper;
+
+class AdminLteUpdateCommand extends Command
+{
+    protected $signature = 'adminlte:update ';
+
+    protected $description = 'Updated the all the required assets for AdminLTE';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('adminlte:install', [
+            '--force' => true, '--only' => 'assets'
+        ]);
+    }
+}
