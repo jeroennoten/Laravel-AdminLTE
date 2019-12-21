7c2e47b671d69b65c5941bf685f1cea2c48c57c8
abasjo <hosch@mailbox.org>
2019-12-21 11:38:17 +0100

    added verification text in german (#448)

--- resources/lang/de/adminlte.php
+++ resources/lang/de/adminlte.php
@@ -19,4 +19,9 @@ return [
     'password_reset_message'      => 'Bitte geben Sie Ihre E-Mail Adresse ein, um Ihr Passwort zurückzusetzen',
     'reset_password'              => 'Passwort zurücksetzen',
     'send_password_reset_link'    => 'Link zur Passwortwiederherstellung senden',
+    'verify_message'              => 'Ihr Account muss noch bestätigt werden',
+    'verify_email_sent'           => 'Es wurde ein neuer Bestätigungslink an Ihre E-Mail Adresse gesendet.',
+    'verify_check_your_email'     => 'Bevor Sie fortfahren, überprüfen Sie bitte Ihre E-Mail auf einen Bestätigungslink.',
+    'verify_if_not_recieved'      => 'Wenn Sie die E-Mail nicht empfangen haben',
+    'verify_request_another'      => 'klicken Sie hier, um eine neue E-Mail anzufordern',
 ];
