6290b5aef439a4f16d22557ac1df4f8b1b0f4a19
Dean <db_newman@hotmail.com>
2019-12-21 20:38:36 +1000

    Updated datatables defaults to apply bootstrap4 (#435)
    Current version did not have support for the bootstrap4 css files.

--- config/adminlte.php
+++ config/adminlte.php
@@ -296,12 +296,17 @@ return [
                 [
                     'type' => 'js',
                     'asset' => false,
-                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js',
+                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
                 ],
                 [
                     'type' => 'css',
                     'asset' => false,
-                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css',
+                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
                 ],
             ],
         ],
