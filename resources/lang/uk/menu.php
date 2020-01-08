002715cc579f0af01faaa0ea59ea2317a34b5900
Alex Stepanov <stteper@mail.ru>
2020-01-08 16:38:49 +0300

    Added translation for Menu (ru and uk)

---
+++ resources/lang/uk/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'ГОЛОВНЕ МЕНЮ',
+    'blog'                          => 'Блог',
+    'pages'                         => 'Сторінки',
+    'account_settings'              => 'НАЛАШТУВАННЯ ПРОФІЛЮ',
+    'profile'                       => 'Профіль',
+    'change_password'               => 'Змінити пароль',
+    'multilevel'                    => 'Багаторівневе меню',
+    'level_one'                     => 'Рівень 1',
+    'level_two'                     => 'Рівень 2',
+    'level_three'                   => 'Рівень 3',
+    'labels'                        => 'Мітки',
+    'important'                     => 'Важливо',
+    'warning'                       => 'Увага',
+    'information'                   => 'Інформація',
+];
