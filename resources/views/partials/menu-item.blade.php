f501d829df48d7386e2a5c1b3d45b62a96d24bbe
Fernando Salles Claro <fsclaro@gmail.com>
2020-01-31 10:24:33 -0300

    implement parameter shift to left shift item on submenu (#465)

--- resources/views/partials/menu-item.blade.php
+++ resources/views/partials/menu-item.blade.php
@@ -18,7 +18,7 @@
         </li>
     @else
         <li @if (isset($item['id'])) id="{{ $item['id'] }}" @endif class="nav-item @if (isset($item['submenu'])){{ $item['submenu_class'] }}@endif">
-            <a class="nav-link {{ $item['class'] }}" href="{{ $item['href'] }}"
+            <a class="nav-link {{ $item['class'] }} @if(isset($item['shift'])) {{ $item['shift'] }} @endif" href="{{ $item['href'] }}"
                @if (isset($item['target'])) target="{{ $item['target'] }}" @endif
             >
                 <i class="{{ $item['icon'] ?? 'far fa-fw fa-circle' }} {{ isset($item['icon_color']) ? 'text-' . $item['icon_color'] : '' }}"></i>
