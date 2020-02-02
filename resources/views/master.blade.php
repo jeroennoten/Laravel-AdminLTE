dd48eaaa067502817a48916ace15fe6fd058b2e1
Stenfrank <faguirre@soenac.com>
2020-02-02 16:09:00 -0500

    CSRF is added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
 @yield('title', config('adminlte.title', 'AdminLTE 3'))
 @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
