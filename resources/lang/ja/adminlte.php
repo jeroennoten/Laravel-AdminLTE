d262013147c2486c2bb34b771fcefd99370689bc
ShibuyaKosuke <shibuya@imagenious.co.jp>
2020-04-18 16:39:53 +0900

    add japanese translation file.

---
+++ resources/lang/ja/adminlte.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'full_name'                   => '氏名',
+    'email'                       => 'メールアドレス',
+    'password'                    => 'パスワード',
+    'retype_password'             => 'もう一度入力',
+    'remember_me'                 => 'パスワードを記憶する',
+    'register'                    => '登録する',
+    'register_a_new_membership'   => 'アカウントを登録する',
+    'i_forgot_my_password'        => 'パスワードを忘れた',
+    'i_already_have_a_membership' => 'すでにアカウントを持っている',
+    'sign_in'                     => 'ログイン',
+    'log_out'                     => 'ログアウト',
+    'toggle_navigation'           => 'ナビゲーションを開閉',
+    'login_message'               => 'ログインしてセッションを開始する',
+    'register_message'            => 'アカウントを登録する',
+    'password_reset_message'      => 'パスワードをリセットする',
+    'reset_password'              => 'パスワードをリセットする',
+    'send_password_reset_link'    => 'パスワードリセットリンクを送信する。',
+    'verify_message'              => 'あなたのアカウントは認証が必要です。',
+    'verify_email_sent'           => 'あなたのメールアドレスに認証用のリンクを送信しました。',
+    'verify_check_your_email'     => '続行する前に、認証用リンクについてメールを確認してください。',
+    'verify_if_not_recieved'      => 'メールが届かない場合',
+    'verify_request_another'      => 'ここをクリックしてもう一度送信する',
+];
