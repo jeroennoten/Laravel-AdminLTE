c722af2ea98c6163d5c5b7c4b64a566e0256dd1a
Diego Smania <63609705+Shidersz@users.noreply.github.com>
2020-04-23 13:51:53 -0300

    Improve the logic to fill the sections classes_body and body_data of the page blade template (#531)
    * [Resources/Views]: Improvements on the logic used to fill the sections classes_body and body_data of the page blade template.
    
    * [Resources/Views]: Fixs reference to a non-existent variable.
    
    * move php functions to adminlte methods
    
    * fix cs
    
    * fix cs
    
    * fixes
    
    Co-authored-by: Florian Ressel <florian@genxtreme.de>

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,41 +5,9 @@
     @yield('css')
 @stop
 
-@section('classes_body',
-    (config('adminlte.sidebar_mini', true) === true ?
-        'sidebar-mini ' :
-        (config('adminlte.sidebar_mini', true) == 'md' ?
-         'sidebar-mini sidebar-mini-md ' : '')
-    ) .
-    (config('adminlte.layout_topnav') || View::getSection('layout_topnav') ? 'layout-top-nav ' : '') .
-    (config('adminlte.layout_boxed') ? 'layout-boxed ' : '') .
-    (!config('adminlte.layout_topnav') && !View::getSection('layout_topnav') ?
-        (config('adminlte.layout_fixed_sidebar') ? 'layout-fixed ' : '') .
-        (config('adminlte.layout_fixed_navbar') === true ?
-            'layout-navbar-fixed ' :
-            (isset(config('adminlte.layout_fixed_navbar')['xs']) ? (config('adminlte.layout_fixed_navbar')['xs'] == true ? 'layout-navbar-fixed ' : 'layout-navbar-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_navbar')['sm']) ? (config('adminlte.layout_fixed_navbar')['sm'] == true ? 'layout-sm-navbar-fixed ' : 'layout-sm-navbar-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_navbar')['md']) ? (config('adminlte.layout_fixed_navbar')['md'] == true ? 'layout-md-navbar-fixed ' : 'layout-md-navbar-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_navbar')['lg']) ? (config('adminlte.layout_fixed_navbar')['lg'] == true ? 'layout-lg-navbar-fixed ' : 'layout-lg-navbar-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_navbar')['xl']) ? (config('adminlte.layout_fixed_navbar')['xl'] == true ? 'layout-xl-navbar-fixed ' : 'layout-xl-navbar-not-fixed ') : '')
-        ) .
-        (config('adminlte.layout_fixed_footer') === true ?
-            'layout-footer-fixed ' :
-            (isset(config('adminlte.layout_fixed_footer')['xs']) ? (config('adminlte.layout_fixed_footer')['xs'] == true ? 'layout-footer-fixed ' : 'layout-footer-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_footer')['sm']) ? (config('adminlte.layout_fixed_footer')['sm'] == true ? 'layout-sm-footer-fixed ' : 'layout-sm-footer-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_footer')['md']) ? (config('adminlte.layout_fixed_footer')['md'] == true ? 'layout-md-footer-fixed ' : 'layout-md-footer-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_footer')['lg']) ? (config('adminlte.layout_fixed_footer')['lg'] == true ? 'layout-lg-footer-fixed ' : 'layout-lg-footer-not-fixed ') : '') .
-            (isset(config('adminlte.layout_fixed_footer')['xl']) ? (config('adminlte.layout_fixed_footer')['xl'] == true ? 'layout-xl-footer-fixed ' : 'layout-xl-footer-not-fixed ') : '')
-        )
-        : ''
-    ) .
-    (config('adminlte.sidebar_collapse') || View::getSection('sidebar_collapse') ? 'sidebar-collapse ' : '') .
-    (config('adminlte.right_sidebar') && config('adminlte.right_sidebar_push') ? 'control-sidebar-push ' : '') .
-    config('adminlte.classes_body')
-)
+@section('classes_body', $adminlte->getBodyClasses())
 
-@section('body_data',
-(config('adminlte.sidebar_scrollbar_theme', 'os-theme-light') != 'os-theme-light' ? 'data-scrollbar-theme=' . config('adminlte.sidebar_scrollbar_theme')  : '') . ' ' . (config('adminlte.sidebar_scrollbar_auto_hide', 'l') != 'l' ? 'data-scrollbar-auto-hide=' . config('adminlte.sidebar_scrollbar_auto_hide')   : ''))
+@section('body_data', $adminlte->getBodyData())
 
 @php( $logout_url = View::getSection('logout_url') ?? config('adminlte.logout_url', 'logout') )
 @php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
