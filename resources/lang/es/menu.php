28008baf94f9be387fd134003f2b5cf589bf0dde
Diego Smania <63609705+Shidersz@users.noreply.github.com>
2020-04-24 19:08:37 -0300

    [Resources/Lang]: Review and complete the spanish translations. (#559)

---
+++ resources/lang/es/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MENU PRINCIPAL',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Páginas',
+    'account_settings'              => 'AJUSTES DE LA CUENTA',
+    'profile'                       => 'Perfil',
+    'change_password'               => 'Cambiar Contraseña',
+    'multilevel'                    => 'Multi Nivel',
+    'level_one'                     => 'Nivel 1',
+    'level_two'                     => 'Nivel 2',
+    'level_three'                   => 'Nivel 3',
+    'labels'                        => 'ETIQUETAS',
+    'important'                     => 'Importante',
+    'warning'                       => 'Advertencia',
+    'information'                   => 'Información',
+];
