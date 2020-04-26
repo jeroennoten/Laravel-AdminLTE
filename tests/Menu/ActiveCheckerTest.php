e19444015c42795926c44410550be8fe18e64d13
Florian Ressel <florian@genxtreme.de>
2020-04-26 21:32:21 +0200

    Fix active filter for urls with query string (#563)
    * fix active checker for menu entries
    
    * fix logic
    
    * change active filter for urls with query string
    
    * change test cases for urls with params

--- tests/Menu/ActiveCheckerTest.php
+++ tests/Menu/ActiveCheckerTest.php
@@ -131,14 +131,14 @@ class ActiveCheckerTest extends TestCase
     {
         $checker = $this->makeActiveChecker('http://example.com/menu?param=option');
 
-        $this->assertTrue($checker->isActive(['url' => 'menu?*']));
+        $this->assertTrue($checker->isActive(['url' => 'menu']));
     }
 
     public function testSubParams()
     {
         $checker = $this->makeActiveChecker('http://example.com/menu/item1?param=option');
 
-        $this->assertTrue($checker->isActive(['url' => 'menu/item1?*']));
+        $this->assertTrue($checker->isActive(['url' => 'menu/item1']));
     }
 
     public function testExplicitActiveRegexEvaluation()
