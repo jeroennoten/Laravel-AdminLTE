1ab2e9323c126b9077d25ba7357923c6dae782ac
Diego Smania <diego.smania@gmail.com>
2020-05-07 17:10:58 -0300

    [Resources/Lang]: Adds a missing message that is used by the confirm password blade.

--- resources/lang/en/adminlte.php
+++ resources/lang/en/adminlte.php
@@ -20,8 +20,9 @@ return [
     'reset_password'              => 'Reset Password',
     'send_password_reset_link'    => 'Send Password Reset Link',
     'verify_message'              => 'Your account needs a verification',
-    'verify_email_sent'                => 'A fresh verification link has been sent to your email address.',
+    'verify_email_sent'           => 'A fresh verification link has been sent to your email address.',
     'verify_check_your_email'     => 'Before proceeding, please check your email for a verification link.',
     'verify_if_not_recieved'      => 'If you did not receive the email',
     'verify_request_another'      => 'click here to request another',
+    'confirm_password_message'    => 'Please, confirm your password to continue.',
 ];
