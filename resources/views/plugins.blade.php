f792428f97694c7feb1c65c300a5fce5aac36b30
Diego Smania <diego.smania@gmail.com>
2020-05-18 15:56:31 -0300

    [Resources/Views]: Improve the master layout. Remove some duplicated (by purposes) sections.

--- resources/views/plugins.blade.php
+++ resources/views/plugins.blade.php
@@ -1,21 +1,14 @@
 @foreach(config('adminlte.plugins') as $plugin)
     @if($plugin['active'] || View::getSection('plugins.' . $plugin['name']))
         @foreach($plugin['files'] as $file)
-            @if($file['type'] == $type)
-                @if($type == 'css')
-                    @if($file['asset'])
-                        <link rel="stylesheet" href="{{ asset($file['location']) }}">
-                    @else
-                        <link rel="stylesheet" href="{{$file['location']}}">
-                    @endif
-                @elseif($type == 'js')
-                    @if($file['asset'])
-                        <script src="{{ asset($file['location']) }}"></script>
-                    @else
-                        <script src="{{$file['location']}}"></script>
-                    @endif
-                @endif
+
+            {{-- Check requested file type --}}
+            @if($file['type'] == $type && $type == 'css')
+                <link rel="stylesheet" href="{{ $file['asset'] ? asset($file['location']) : $file['location'] }}">
+            @elseif($file['type'] == $type && $type == 'js')
+                <script src="{{ $file['asset'] ? asset($file['location']) : $file['location'] }}"></script>
             @endif
+
         @endforeach
     @endif
 @endforeach
