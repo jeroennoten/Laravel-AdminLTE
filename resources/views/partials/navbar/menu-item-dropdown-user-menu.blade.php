4a7589cd90ebcbfd3fbbbcc7fc201ee5086f703f
Florian Ressel <florian@genxtreme.de>
2020-05-27 12:02:05 +0200

    change condition

--- resources/views/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -1,7 +1,7 @@
 @php( $logout_url = View::getSection('logout_url') ?? config('adminlte.logout_url', 'logout') )
 @php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
 
-@if (config('adminlte.usermenu_profile_url'))
+@if (config('adminlte.usermenu_profile_url', false))
     @php( $profile_url = Auth::user()->adminlte_profile_url() )
 @endif
 
