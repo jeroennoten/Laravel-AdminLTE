a0df5a3e0205cf9223b50fe746ebe4239a98e995
Diego Smania <diego.smania@gmail.com>
2020-05-28 18:54:50 -0300

    [src/AdminLte]: Add a new Layout helper and use it to get body classes and data attributes.

--- src/AdminLte.php
+++ src/AdminLte.php
@@ -4,10 +4,10 @@ namespace JeroenNoten\LaravelAdminLte;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Support\Facades\View;
 use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
-use JeroenNoten\LaravelAdminLte\Menu\Builder;
 use JeroenNoten\LaravelAdminLte\Helpers\MenuItemHelper;
+use JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper;
+use JeroenNoten\LaravelAdminLte\Menu\Builder;
 
 class AdminLte
 {
@@ -66,100 +66,7 @@ class AdminLte
      */
     public function getBodyClasses()
     {
-        $body_classes = [];
-        $screen_sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
-
-        // Add classes related to the "sidebar_mini" configuration.
-
-        if (config('adminlte.sidebar_mini', true) === true) {
-            $body_classes[] = 'sidebar-mini';
-        } elseif (config('adminlte.sidebar_mini', true) == 'md') {
-            $body_classes[] = 'sidebar-mini sidebar-mini-md';
-        }
-
-        // Add classes related to the "layout_topnav" configuration.
-
-        if (config('adminlte.layout_topnav') || View::getSection('layout_topnav')) {
-            $body_classes[] = 'layout-top-nav';
-        }
-
-        // Add classes related to the "layout_boxed" configuration.
-
-        if (config('adminlte.layout_boxed') || View::getSection('layout_boxed')) {
-            $body_classes[] = 'layout-boxed';
-        }
-
-        // Add classes related to the "sidebar_collapse" configuration.
-
-        if (config('adminlte.sidebar_collapse') || View::getSection('sidebar_collapse')) {
-            $body_classes[] = 'sidebar-collapse';
-        }
-
-        // Add classes related to the "right_sidebar" configuration.
-
-        if (config('adminlte.right_sidebar') && config('adminlte.right_sidebar_push')) {
-            $body_classes[] = 'control-sidebar-push';
-        }
-
-        // Add classes related to fixed sidebar, these are not compatible with
-        // "layout_topnav".
-
-        if (! config('adminlte.layout_topnav') && ! View::getSection('layout_topnav')) {
-
-            // Check for fixed sidebar configuration.
-
-            if (config('adminlte.layout_fixed_sidebar')) {
-                $body_classes[] = 'layout-fixed';
-            }
-        }
-
-        // Add classes related to fixed footer and navbar, these are not
-        // compatible with "layout_boxed".
-
-        if (! config('adminlte.layout_boxed') && ! View::getSection('layout_boxed')) {
-
-            // Check for fixed navbar configuration.
-
-            $fixed_navbar_cfg = config('adminlte.layout_fixed_navbar');
-
-            if ($fixed_navbar_cfg === true) {
-                $body_classes[] = 'layout-navbar-fixed';
-            } elseif (is_array($fixed_navbar_cfg)) {
-                foreach ($fixed_navbar_cfg as $size => $enabled) {
-                    if (in_array($size, $screen_sizes)) {
-                        $size = $size == 'xs' ? '' : '-'.$size;
-                        $body_classes[] = $enabled == true ?
-                            'layout'.$size.'-navbar-fixed' :
-                            'layout'.$size.'-navbar-not-fixed';
-                    }
-                }
-            }
-
-            // Check for fixed footer configuration.
-
-            $fixed_footer_cfg = config('adminlte.layout_fixed_footer');
-
-            if ($fixed_footer_cfg === true) {
-                $body_classes[] = 'layout-footer-fixed';
-            } elseif (is_array($fixed_footer_cfg)) {
-                foreach ($fixed_footer_cfg as $size => $enabled) {
-                    if (in_array($size, $screen_sizes)) {
-                        $size = $size == 'xs' ? '' : '-'.$size;
-                        $body_classes[] = $enabled == true ?
-                            'layout'.$size.'-footer-fixed' :
-                            'layout'.$size.'-footer-not-fixed';
-                    }
-                }
-            }
-        }
-
-        // Add custom classes, related to the "classes_body" configuration.
-
-        $body_classes[] = config('adminlte.classes_body', '');
-
-        // Return the set of configured classes for the body tag.
-
-        return trim(implode(' ', $body_classes));
+        return trim(implode(' ', LayoutHelper::makeBodyClasses()));
     }
 
     /**
@@ -167,25 +74,7 @@ class AdminLte
      */
     public function getBodyData()
     {
-        $body_data = [];
-
-        // Add data related to the "sidebar_scrollbar_theme" configuration.
-
-        $sb_theme_cfg = config('adminlte.sidebar_scrollbar_theme', 'os-theme-light');
-
-        if ($sb_theme_cfg != 'os-theme-light') {
-            $body_data[] = 'data-scrollbar-theme='.$sb_theme_cfg;
-        }
-
-        // Add data related to the "sidebar_scrollbar_auto_hide" configuration.
-
-        $sb_auto_hide = config('adminlte.sidebar_scrollbar_auto_hide', 'l');
-
-        if ($sb_auto_hide != 'l') {
-            $body_data[] = 'data-scrollbar-auto-hide='.$sb_auto_hide;
-        }
-
-        return trim(implode(' ', $body_data));
+        return trim(implode(' ', LayoutHelper::makeBodyData()));
     }
 
     protected function buildMenu()
