82e7a3f14a7de6e0a7bb33a1e659f87f024261a5
Diego Smania <63609705+Shidersz@users.noreply.github.com>
2020-06-09 02:51:41 -0300

    [General]: Change default value for config classes_topnav_nav to solve misplacing of toggle button on small screens (#627)

--- config/adminlte.php
+++ config/adminlte.php
@@ -129,7 +129,7 @@ return [
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
-    'classes_topnav_nav' => 'navbar-expand-md',
+    'classes_topnav_nav' => 'navbar-expand',
     'classes_topnav_container' => 'container',
 
     /*
