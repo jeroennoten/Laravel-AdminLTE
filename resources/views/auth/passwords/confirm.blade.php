343b9c47cfac3e1c4ec48c9ef60a88265858b969
Diego Smania <diego.smania@gmail.com>
2020-06-10 12:08:57 -0300

    [Resources/Views]: Fix compatibility problem with Laravel Mix.

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -82,7 +82,6 @@
 @stop
 
 @section('adminlte_js')
-    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     @stack('js')
     @yield('js')
 @stop
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -77,6 +77,8 @@
 
         {{-- Configured Scripts --}}
         @include('adminlte::plugins', ['type' => 'js'])
+
+        <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     @else
         <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
     @endif
