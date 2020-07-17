8c38003cbc7a724af6d24759e189611810539d89
Florian Ressel <florian@genxtreme.de>
2020-07-17 19:31:19 +0200

    fix issue #670

--- resources/views/partials/sidebar/menu-item-search-form.blade.php
+++ resources/views/partials/sidebar/menu-item-search-form.blade.php
@@ -1,6 +1,8 @@
 <li @if(isset($item['id'])) id="{{ $item['id'] }}" @endif>
 
     <form action="{{ $item['href'] }}" method="{{ $item['method'] }}" class="form-inline">
+        {{ csrf_field() }}
+        
         <div class="input-group">
             <input class="form-control form-control-sidebar" type="search"
                    name="{{ $item['input_name'] }}" placeholder="{{ $item['text'] }}"
