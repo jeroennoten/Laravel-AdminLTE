ef29194e031761615c239d6b16b3351717e2fe40
arcphysx <raymondpurnomo@gmail.com>
2020-09-14 13:08:13 +0700

    Add Bahasa Indonesia Translation

---
+++ resources/lang/id/adminlte.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nama lengkap',
+    'email'                       => 'Email',
+    'password'                    => 'Kata sandi',
+    'retype_password'             => 'Ketik ulang kata sandi',
+    'remember_me'                 => 'Ingat Saya',
+    'register'                    => 'Daftar',
+    'register_a_new_membership'   => 'Daftar sebagai anggota baru',
+    'i_forgot_my_password'        => 'Saya lupa kata sandi',
+    'i_already_have_a_membership' => 'Saya telah menjadi anggota',
+    'sign_in'                     => 'Masuk',
+    'log_out'                     => 'Keluar',
+    'toggle_navigation'           => 'Toggle navigasi',
+    'login_message'               => 'Masuk untuk memulai sesi Anda',
+    'register_message'            => 'Daftar sebagai anggota baru',
+    'password_reset_message'      => 'Atur Ulang Kata Sandi',
+    'reset_password'              => 'Atur Ulang Kata Sandi',
+    'send_password_reset_link'    => 'Kirim Tautan Atur Ulang Kata Sandi',
+    'verify_message'              => 'Akun Anda membutuhkan verifikasi',
+    'verify_email_sent'           => 'Tautan verifikasi baru telah dikirimkan ke email Anda.',
+    'verify_check_your_email'     => 'Sebelum melanjutkan, periksa email Anda untuk tautan verifikasi.',
+    'verify_if_not_recieved'      => 'Jika Anda tidak menerima email',
+    'verify_request_another'      => 'klik disini untuk meminta ulang',
+    'confirm_password_message'    => 'Konfirmasi kata sandi Anda untuk melanjutkan',
+];
