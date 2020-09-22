588cba20f6ee628e475a020f9e98d0ee1ef31bc0
@lirez@ <alr2413@gmail.com>
2020-09-23 00:12:03 +0330

    Add support to pass parameters to menu translate function (#710)
    * Support passing parameters to menu translate.
    
    Readme also updated.
    
    * Update .gitignore
    
    * fix StyleCI issue
    
    * .gitignore updated
    
    * Delete .DS_Store
    
    * Add test for menu translation with params
    
    * change cs
    
    Co-authored-by: Florian Ressel <info@k-30.de>

--- src/Menu/Filters/LangFilter.php
+++ src/Menu/Filters/LangFilter.php
@@ -42,8 +42,13 @@ class LangFilter implements FilterInterface
         // Translate the menu item properties.
 
         foreach ($this->itemProperties as $prop) {
-            if (isset($item[$prop]) && is_string($item[$prop])) {
-                $item[$prop] = $this->getTranslation($item[$prop]);
+            if (isset($item[$prop])) {
+                if (is_array($item[$prop])) {
+                    $params = (isset($item[$prop][1]) && is_array($item[$prop][1])) ? $item[$prop][1] : [];
+                    $item[$prop] = $this->getTranslation($item[$prop][0], $params);
+                } elseif (is_string($item[$prop])) {
+                    $item[$prop] = $this->getTranslation($item[$prop]);
+                }
             }
         }
 
@@ -54,16 +59,17 @@ class LangFilter implements FilterInterface
      * Gets the translation for a given key.
      *
      * @param string $key The key to translate
+     * @param array $params The additional translation params
      * @return string The translation
      */
-    protected function getTranslation($key)
+    protected function getTranslation($key, $params = [])
     {
         // Check for a translation.
 
         if ($this->translator->has('menu.'.$key)) {
-            return $this->translator->get('menu.'.$key);
+            return $this->translator->get('menu.'.$key, $params);
         } elseif ($this->translator->has('adminlte::menu.'.$key)) {
-            return $this->translator->get('adminlte::menu.'.$key);
+            return $this->translator->get('adminlte::menu.'.$key, $params);
         }
 
         // When no translation available, return the original key.
