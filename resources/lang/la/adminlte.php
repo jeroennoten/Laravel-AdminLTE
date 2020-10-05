57a051c5a20f748fede8e9882470713c875ca176
TIMMYPPV <thim_ppv@icloud.com>
2020-10-05 22:12:43 +0700

    Add Lao Language

--- resources/lang/la/adminlte.php
+++ resources/lang/la/adminlte.php
@@ -3,7 +3,7 @@
 return [
 
     'full_name'                   => 'ຊື່',
-    'email'                       => 'ອີເມລ',
+    'email'                       => 'ອີເມວ',
     'password'                    => 'ລະຫັດຜ່ານ',
     'retype_password'             => 'ພິມລະຫັດຜ່ານອີກຄັ້ງ',
     'remember_me'                 => 'ຈື່ຂ້ອຍໄວ້',
@@ -19,4 +19,10 @@ return [
     'password_reset_message'      => 'ລະຫັດລີເຊັດຂໍ້ຄວາມ',
     'reset_password'              => 'ລີເຊັດຂໍ້ຄວາມ',
     'send_password_reset_link'    => 'ສົ່ງລິ້ງລີເຊັດລະຫັດຜ່ານ',
+    'verify_message'              => 'ບັນຊີຂອງທ່ານຕ້ອງໄດ້ຮັບການຢືນຢັນ',
+    'verify_email_sent'           => 'Link ການຢືນຢັນບັນຊີຖືກສົ່ງເຂົ້າອີເມວຂອງທ່ານແລ້ວ',
+    'verify_check_your_email'     => 'ກ່ອນດຳເນີນການ, ທ່ານກະລຸນາກວດສອບອີເມວເພື່ອຢືນຢັນກ່ອນ',
+    'verify_if_not_recieved'      => 'ຂ້າພະເຈົ້າບໍ່ໄດ້ຮັບ ອີເມວ',
+    'verify_request_another'      => 'ກົດທີ່ນີ້ເພື່ອຮັບອີເມວອີກເທື່ອ',
+    'confirm_password_message'    => 'ກະລຸນາຢືນຢັນລະຫັດທ່ານເພື່ອທຳການຢືນຢັນ',
 ];
