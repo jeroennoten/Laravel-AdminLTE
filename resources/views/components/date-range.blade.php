48eea77cb7461b989b06dd243ebf5075a17341cd
Diego Smania <diego.smania@gmail.com>
2020-10-19 15:38:48 -0300

    [Components]: Improve the date-range component, now extends from input group component.

--- resources/views/components/date-range.blade.php
+++ resources/views/components/date-range.blade.php
@@ -1,36 +1,104 @@
-<div class="filter d-flex {{$topclass}}">
-    <div class="form-group mr-1">
-        <div class="input-group">
-            <button type="button" class="btn btn-default float-right {{$inputclass}}" id="{{$id}}">
-                <i class="{{$icon}}"></i> {{$title}}
-                <i class="fas fa-caret-down"></i>
-            </button>
-        </div>
-    </div>
-    <div class="form-group ml-1" style="flex: 1 0 auto">
-        <input type="text" class="form-control" id="range-result" style="border:0;background:transparent" disabled>
-    </div>
-</div>
+@extends('adminlte::components.input-group-component')
 
-@section('js')
-@parent 
+@section('input_group_item')
+
+    {{-- Date Range Input --}}
+    <input id="{{ $name }}" name="{{ $name }}"
+        {{ $attributes->merge(['class' => $makeItemClass($errors->first($name))]) }}>
+
+@overwrite
+
+{{-- Add plugin initialization and configuration code --}}
+
+@push('js')
 <script>
-    $('#{{$id}}').daterangepicker({
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        dateLimit: {
-            months: 12
-        },
-        {!!$initiator!!}
-    }, function (start, end) {
-        $('#range-result').val('Showing results: from [' + start.format('YYYY-MM-DD') + '] to [' + end.format('YYYY-MM-DD') + ']');
-        {!!$callback!!}
-    });
+
+    $(() => {
+        let usrCfg = _adminlte_drUtils.parseCfg( @json($config) );
+
+        // Check if default set of ranges should be enabled.
+
+        @isset($enableDefaultRanges)
+            usrCfg.ranges = usrCfg.ranges || _adminlte_drUtils.defRanges();
+            let range = usrCfg.ranges['{{ $enableDefaultRanges }}'];
+
+            if (Array.isArray(range)) {
+                usrCfg.startDate = range[0];
+                usrCfg.endDate = range[1];
+            }
+        @endisset
+
+        $('#{{ $name }}').daterangepicker(usrCfg);
+    })
+
 </script>
-@endsection
+@endpush
+
+{{-- Add utility methods for the plugin --}}
+
+@once
+@push('js')
+<script>
+
+    function DR_Utils() {
+
+        /**
+         * Parse the php plugin configuration to eval javascript code.
+         */
+        this.parseCfg = function(cfg)
+        {
+            for (const prop in cfg) {
+                let v = cfg[prop];
+
+                if (typeof v === 'string' && v.startsWith('js:')) {
+                    cfg[prop] = eval(v.slice(3));
+                }
+                else if (typeof v === 'object') {
+                    cfg[prop] = this.parseCfg(v);
+                }
+            }
+
+            return cfg;
+        }
+
+        /**
+         * Creates a default set of ranges option for the plugin.
+         */
+        this.defRanges = function()
+        {
+            return {
+                'Today': [
+                    moment(),
+                    moment()
+                ],
+                'Yesterday': [
+                    moment().subtract(1, 'days'),
+                    moment().subtract(1, 'days')
+                ],
+                'Last 7 Days': [
+                    moment().subtract(6, 'days'),
+                    moment()
+                ],
+                'Last 30 Days': [
+                    moment().subtract(29, 'days'),
+                    moment()
+                ],
+                'This Month': [
+                    moment().startOf('month'),
+                    moment().endOf('month')
+                ],
+                'Last Month': [
+                    moment().subtract(1, 'month').startOf('month'),
+                    moment().subtract(1, 'month').endOf('month')
+                ]
+            };
+        }
+    }
+
+    // Create the plugin utililities object.
+
+    var _adminlte_drUtils = new DR_Utils();
+
+</script>
+@endpush
+@endonce
