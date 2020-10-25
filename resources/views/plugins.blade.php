6216afa13b9d2c76846db31079b3a6d1c627d7da
Diego Smania <diego.smania@gmail.com>
2020-10-25 14:39:55 -0300

    [resources/views]: Improve code logic on the plugins blade file.

--- resources/views/plugins.blade.php
+++ resources/views/plugins.blade.php
@@ -2,11 +2,18 @@
     @if($plugin['active'] || View::getSection('plugins.' . ($plugin['name'] ?? $pluginName)))
         @foreach($plugin['files'] as $file)
 
+            {{-- Setup the file location  --}}
+            @php
+                if (! empty($file['asset'])) {
+                    $file['location'] = asset($file['location']);
+                }
+            @endphp
+
             {{-- Check requested file type --}}
             @if($file['type'] == $type && $type == 'css')
-                <link rel="stylesheet" href="{{ $file['asset'] ? asset($file['location']) : $file['location'] }}">
+                <link rel="stylesheet" href="{{ $file['location'] }}">
             @elseif($file['type'] == $type && $type == 'js')
-                <script src="{{ $file['asset'] ? asset($file['location']) : $file['location'] }}" {{ ! empty($file['defer']) ? 'defer' : '' }} ></script>
+                <script src="{{ $file['location'] }}" @if(! empty($file['defer'])) defer @endif></script>
             @endif
 
         @endforeach
