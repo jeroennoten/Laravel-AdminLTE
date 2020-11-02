4f6b2d6322135075be9a5c798ecaee6df6db32f6
Diego Smania <63609705+Shidersz@users.noreply.github.com>
2020-11-02 04:30:43 -0300

    [Components]: Improve the input-switch component, now extends from input group component. (#755)

--- resources/views/components/input-switch.blade.php
+++ resources/views/components/input-switch.blade.php
@@ -1,10 +1,52 @@
-<div class="form-group {{$topclass}}">
-    <div class="custom-control custom-switch">
-        <input type="checkbox" class="custom-control-input {{$inputclass}}" 
-        id="{{$id}}" name="{{$name}}" 
-        {{$checked ? 'checked' : ''}}
-        {{$disabled ? 'disabled' : ''}}
-        {{$required ? 'required' : ''}}>
-        <label class="custom-control-label" for="{{$id}}">{{$label}}</label>
-    </div>
-</div>
+@extends('adminlte::components.input-group-component')
+
+@section('input_group_item')
+
+    {{-- Input Switch --}}
+    <input type="checkbox" id="{{ $name }}" name="{{ $name }}"
+        {{ $attributes->merge(['class' => $makeItemClass($errors->first($name))]) }}>
+
+@overwrite
+
+{{-- Add plugin initialization and configuration code --}}
+
+@push('js')
+<script>
+
+    $(() => {
+        $('#{{ $name }}').bootstrapSwitch( @json($config) );
+    })
+
+</script>
+@endpush
+
+{{-- Setup the height/font of the plugin when using sm/lg sizes --}}
+{{-- NOTE: this may change with newer plugin versions --}}
+
+@once
+@push('css')
+<style type="text/css">
+
+    {{-- MD (default) size setup --}}
+    .input-group .bootstrap-switch-handle-on,
+    .input-group .bootstrap-switch-handle-off {
+        height: 2.25rem !important;
+    }
+
+    {{-- LG size setup --}}
+    .input-group-lg .bootstrap-switch-handle-on,
+    .input-group-lg .bootstrap-switch-handle-off {
+        height: 2.875rem !important;
+        font-size: 1.25rem !important;
+    }
+
+    {{-- SM size setup --}}
+    .input-group-sm .bootstrap-switch-handle-on,
+    .input-group-sm .bootstrap-switch-handle-off {
+        height: 1.8125rem !important;
+        font-size: .875rem !important;
+    }
+
+</style>
+@endpush
+@endonce
--- src/Components/InputSwitch.php
+++ src/Components/InputSwitch.php
@@ -2,35 +2,56 @@
 
 namespace JeroenNoten\LaravelAdminLte\Components;
 
-use Illuminate\View\Component;
-
-class InputSwitch extends Component
+class InputSwitch extends InputGroupComponent
 {
-    public $id;
-    public $name;
-    public $label;
-    public $topclass;
-    public $inputclass;
-    public $checked;
-    public $disabled;
-    public $required;
+    /**
+     * The Bootstrap Switch plugin configuration parameters. Array with
+     * key => value pairs, where the key should be an existing configuration
+     * property of the plugin.
+     *
+     * @var array
+     */
+    public $config;
 
+    /**
+     * Create a new component instance.
+     * Note this component requires the 'Bootstrap Switch' plugin.
+     *
+     * @return void
+     */
     public function __construct(
-            $id = 'checkbox', $name = null,
-            $label = 'Input Label',
-            $topclass = null, $inputclass = null,
-            $checked = false, $disabled = false, $required = false
-        ) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->label = $label;
-        $this->topclass = $topclass;
-        $this->inputclass = $inputclass;
-        $this->checked = $checked;
-        $this->required = $required;
-        $this->disabled = $disabled;
+        $name, $label = null, $size = null, $labelClass = null,
+        $topClass = null, $disableFeedback = null, $config = []
+    ) {
+        parent::__construct(
+            $name, $label, $size, $labelClass, $topClass, $disableFeedback
+        );
+
+        $this->config = is_array($config) ? $config : [];
     }
 
+    /**
+     * Make the class attribute for the input group item.
+     *
+     * @param string $invalid
+     * @return string
+     */
+    public function makeItemClass($invalid)
+    {
+        $classes = [];
+
+        if (! empty($invalid) && ! isset($this->disableFeedback)) {
+            $classes[] = 'is-invalid';
+        }
+
+        return implode(' ', $classes);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
     public function render()
     {
         return view('adminlte::components.input-switch');
