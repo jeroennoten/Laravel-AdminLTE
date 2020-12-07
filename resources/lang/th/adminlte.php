cbaf64742c371fb7759278a26196f5975afe3b88
BazioDEV <bazio.apple@gmail.com>
2020-12-07 16:56:12 +0700

    Add thai language in th folder

---
+++ resources/lang/th/adminlte.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'full_name'                   => 'ชื่อ-นามสกุล',
+    'email'                       => 'อีเมล',
+    'password'                    => 'รหัสผ่าน',
+    'retype_password'             => 'พิมรหัสอีกครั้ง',
+    'remember_me'                 => 'จดจำ',
+    'register'                    => 'สมัคร',
+    'register_a_new_membership'   => 'สมัครสมาชิคใหม่',
+    'i_forgot_my_password'        => 'ลืมรหัสผ่าน',
+    'i_already_have_a_membership' => 'ฉันเป็นสมาชิคแล้ว',
+    'sign_in'                     => 'ลงชื่อเข้า',
+    'log_out'                     => 'ลงชื่อออก',
+    'toggle_navigation'           => 'Toggle navigation',
+    'login_message'               => 'ลงชื่อเข้าใช้เพื่อเริ่มเซสชันของคุณ',
+    'register_message'            => 'สมัครสมาชิคใหม่',
+    'password_reset_message'      => 'รีเซ็ทรหัสผ่าน',
+    'reset_password'              => 'รีเซ็ทรหัสผ่าน',
+    'send_password_reset_link'    => 'ส่งลิ้งกู้รหัสผ่าน',
+    'verify_message'              => 'บัญชีของคุณยังไม่ได้รับการตรวจสอบ',
+    'verify_email_sent'           => 'ลิ้งการยืนยันบัญชีได้ถูกส่งไปยังอีเมลของคุณแล้ว',
+    'verify_check_your_email'     => 'ก่อนดำเนินต่อ, กรุณาตรวจสอบลิ้งยืนยันบัญชีในอีกเมล',
+    'verify_if_not_recieved'      => 'ถ้าคุณยังไม่ได้รับอีเมล',
+    'verify_request_another'      => 'คลิ๊กที่นี่เพื่อรับอีเมลอีกครั้ง',
+    'confirm_password_message'    => 'กรุณา, ยืนยันรหัสผ่านเพื่อดำเนินการต่อ',
+];
