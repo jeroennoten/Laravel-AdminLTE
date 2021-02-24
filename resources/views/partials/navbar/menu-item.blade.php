9d31a4c1b0284191f1c12fe6aeefa9794b5430e9
Diego Smania <diego.smania@gmail.com>
2021-02-23 23:23:44 -0300

    [Resources/Views]: Fix @injection directive on the blade views files.

--- resources/views/partials/navbar/menu-item.blade.php
+++ resources/views/partials/navbar/menu-item.blade.php
@@ -1,4 +1,4 @@
-@inject('menuItemHelper', \JeroenNoten\LaravelAdminLte\Helpers\MenuItemHelper)
+@inject('menuItemHelper', 'JeroenNoten\LaravelAdminLte\Helpers\MenuItemHelper')
 
 @if ($menuItemHelper->isSearchBar($item))
 
