36eeeffba1a3da7ec7a824cbc8c8fb09fa617901
Stasa <stasavujicic@gmail.com>
2021-04-07 15:34:07 +0200

    Added Serbian Latin localization.

---
+++ resources/lang/sr/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'GLAVNA NAVIGACIJA',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Strane',
+    'account_settings'              => 'PODEŠAVANJA NALOGA',
+    'profile'                       => 'Profil',
+    'change_password'               => 'Promena lozinke',
+    'multilevel'                    => 'Više nivoa',
+    'level_one'                     => 'Nivo 1',
+    'level_two'                     => 'Nivo 2',
+    'level_three'                   => 'Nivo 3',
+    'labels'                        => 'OZNAKE',
+    'important'                     => 'Važno',
+    'warning'                       => 'Upozorenje',
+    'information'                   => 'Informacije',
+    'menu'                          => 'MENI',
+    'users'                         => 'Korisnici',
+];
