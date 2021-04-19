f75ab426a331aa7d826657f9d1e017b3884ad908
Diego Smania <diego.smania@gmail.com>
2021-04-19 15:18:41 -0300

    [Tests]: Fix tests due to new menu item.

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -87,7 +87,7 @@ class ServiceProviderTest extends TestCase
         $adminlte = $this->app->make(AdminLte::class);
         $menu = $adminlte->menu();
 
-        $this->assertCount(10, $menu);
+        $this->assertCount(11, $menu);
         $this->assertEquals('search', $menu[0]['text']);
     }
 
