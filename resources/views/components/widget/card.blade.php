3d68f0ed366fbf574aa541ec27b4704d98db68b7
shankhadevpadam <shankhadev123@gmail.com>
2021-05-23 11:14:49 +0545

    remove card header if title not exists

--- resources/views/components/widget/card.blade.php
+++ resources/views/components/widget/card.blade.php
@@ -1,32 +1,34 @@
 <div {{ $attributes->merge(['class' => $makeCardClass()]) }}>
 
     {{-- Card header --}}
-    <div class="card-header">
+    @if(! empty($title))
+        <div class="card-header">
 
-        {{-- Title --}}
-        <h3 class="{{ $makeCardTitleClass() }}">
-            @isset($icon)<i class="{{ $icon }} mr-2"></i>@endisset
-            @isset($title){{ $title }}@endisset
-        </h3>
+            {{-- Title --}}
+            <h3 class="{{ $makeCardTitleClass() }}">
+                @isset($icon)<i class="{{ $icon }} mr-2"></i>@endisset
+                @isset($title){{ $title }}@endisset
+            </h3>
 
-        {{-- Tools --}}
-        <div class="card-tools">
-            @isset($maximizable)
-                <x-adminlte-button theme="tool" data-card-widget="maximize" icon="fas fa-lg fa-expand"/>
-            @endisset
+            {{-- Tools --}}
+            <div class="card-tools">
+                @isset($maximizable)
+                    <x-adminlte-button theme="tool" data-card-widget="maximize" icon="fas fa-lg fa-expand"/>
+                @endisset
 
-            @if($collapsible === 'collapsed')
-                <x-adminlte-button theme="tool" data-card-widget="collapse" icon="fas fa-lg fa-plus"/>
-            @elseif (isset($collapsible))
-                <x-adminlte-button theme="tool" data-card-widget="collapse" icon="fas fa-lg fa-minus"/>
-            @endif
+                @if($collapsible === 'collapsed')
+                    <x-adminlte-button theme="tool" data-card-widget="collapse" icon="fas fa-lg fa-plus"/>
+                @elseif (isset($collapsible))
+                    <x-adminlte-button theme="tool" data-card-widget="collapse" icon="fas fa-lg fa-minus"/>
+                @endif
 
-            @isset($removable)
-                <x-adminlte-button theme="tool" data-card-widget="remove" icon="fas fa-lg fa-times"/>
-            @endisset
+                @isset($removable)
+                    <x-adminlte-button theme="tool" data-card-widget="remove" icon="fas fa-lg fa-times"/>
+                @endisset
+            </div>
+
         </div>
-
-    </div>
+    @endif
 
     {{-- Card body --}}
     <div class="card-body">{{ $slot }}</div>
