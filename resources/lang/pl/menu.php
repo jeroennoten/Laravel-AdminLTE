c7f1fe543cbfd19ea6c3e51f19369776978c1cf5
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2021-06-02 22:35:36 +0200

    Add more Polish translations

---
+++ resources/lang/pl/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'GŁÓWNA NAWIGACJA',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Strony',
+    'account_settings'              => 'USTAWIENIA KONTA',
+    'profile'                       => 'Profil',
+    'change_password'               => 'Zmień hasło',
+    'multilevel'                    => 'Wielopoziomowe',
+    'level_one'                     => 'Poziom 1',
+    'level_two'                     => 'Poziom 2',
+    'level_three'                   => 'Poziom 3',
+    'labels'                        => 'ETYKIETY',
+    'important'                     => 'Ważne',
+    'warning'                       => 'Ostrzeżenie',
+    'information'                   => 'Informacja',
+];
