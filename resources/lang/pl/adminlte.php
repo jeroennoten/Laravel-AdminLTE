42c010efafe33f161c49df28e0729d5aa8f07f68
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2021-06-03 18:40:38 +0200

    Add more Polish translations (#914)

--- resources/lang/pl/adminlte.php
+++ resources/lang/pl/adminlte.php
@@ -19,4 +19,10 @@ return [
     'password_reset_message'      => 'Resetuj hasło',
     'reset_password'              => 'Resetuj hasło',
     'send_password_reset_link'    => 'Wyślij link do resetowania hasła',
+    'verify_message'              => 'Twoje konto wymaga weryfikacji',
+    'verify_email_sent'           => 'Na Twój adres email został wysłany nowy link weryfikacyjny.',
+    'verify_check_your_email'     => 'Zanim przejdziesz dalej, sprawdź pocztę email pod kątem linku weryfikacyjnego.',
+    'verify_if_not_recieved'      => 'Jeśli nie otrzymałeś emaila',
+    'verify_request_another'      => 'kliknij tutaj, aby poprosić o inny',
+    'confirm_password_message'    => 'Aby kontynuować, proszę potwierdzić swoje hasło.',
 ];
