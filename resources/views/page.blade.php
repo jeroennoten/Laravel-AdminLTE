a052c5ea399f15c7062bb23ab939e3a36b9929fd
warquia <warquia@gmail.com>
2021-06-05 17:01:55 -0300

    Update page.blade.php

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -36,77 +36,7 @@
         <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
 
             {{-- Content Header --}}
-            <div class="{{ config('adminlte.classes_content_header_main') ?: 'content-header' }}">
-                <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
-                    @yield('content_header')
-                </div>
-            </div>
-
-            {{-- Main Content --}}
-            <div class="content">
-                <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
-                    @yield('content')
-                </div>
-            </div>
-
-        </div>
-
-        {{-- Footer --}}
-        @hasSection('footer')
-            @include('adminlte::partials.footer.footer')
-        @endif
-
-        {{-- Right Control Sidebar --}}
-        @if(config('adminlte.right_sidebar'))
-            @include('adminlte::partials.sidebar.right-sidebar')
-        @endif
-
-    </div>
-@stop
-
-@section('adminlte_js')
-    @stack('js')
-    @yield('js')
-@stop
-@extends('adminlte::master')
-
-@inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
-
-@if($layoutHelper->isLayoutTopnavEnabled())
-    @php( $def_container_class = 'container' )
-@else
-    @php( $def_container_class = 'container-fluid' )
-@endif
-
-@section('adminlte_css')
-    @stack('css')
-    @yield('css')
-@stop
-
-@section('classes_body', $layoutHelper->makeBodyClasses())
-
-@section('body_data', $layoutHelper->makeBodyData())
-
-@section('body')
-    <div class="wrapper">
-
-        {{-- Top Navbar --}}
-        @if($layoutHelper->isLayoutTopnavEnabled())
-            @include('adminlte::partials.navbar.navbar-layout-topnav')
-        @else
-            @include('adminlte::partials.navbar.navbar')
-        @endif
-
-        {{-- Left Main Sidebar --}}
-        @if(!$layoutHelper->isLayoutTopnavEnabled())
-            @include('adminlte::partials.sidebar.left-sidebar')
-        @endif
-
-        {{-- Content Wrapper --}}
-        <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
-
-            {{-- Content Header --}}
-            <div class="{{ config('adminlte.classes_content_header_main') ?: 'content-header' }}">
+            <div class="{{ config('adminlte.classes_content_header_main') }}">
                 <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
                     @yield('content_header')
                 </div>
