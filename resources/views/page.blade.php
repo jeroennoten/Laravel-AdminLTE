e6f95bc328e4d28c55b73d31687376d8e1b692ee
warquia <warquia@gmail.com>
2021-06-08 14:06:35 -0300

    Update page.blade.php

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -32,26 +32,12 @@
             @include('adminlte::partials.sidebar.left-sidebar')
         @endif
 
-        {{-- Content Wrapper --}}
-        <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
-
-            {{-- Content Header --}}
-            @hasSection('content_header')
-                <div class="content-header">
-                    <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
-                        @yield('content_header')
-                    </div>
-                </div>
-            @endif
-
-            {{-- Main Content --}}
-            <div class="content">
-                <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
-                    @yield('content')
-                </div>
-            </div>
-
-        </div>
+        {{-- Body of the Page --}}
+        @if(!$layoutHelper->isLayoutIFrameEnabled())
+            @include('adminlte::partials.common.content-base')
+        @else
+            @include('adminlte::partials.iframe.iframe-base')
+        @endif
 
         {{-- Footer --}}
         @hasSection('footer')
