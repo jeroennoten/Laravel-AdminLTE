15e3d93ef51196e570625a2ab8c08e023e4dd96e
Warquia <warquia@gmail.com>
2021-06-09 05:42:29 -0300

    AdminLTE  IFrame (#921)
    * Update page.blade.php
    
    * Update page.blade.php
    
    * Update adminlte.php
    
    * Update page.blade.php
    
    With fallback
    
    * Delete adminlte.php
    
    a better approach was taken
    
    * Update page.blade.php
    
    A better approach was taken
    
    * Update page.blade.php
    
    * Revert "Delete adminlte.php"
    
    This reverts commit 6ab619d22f6a2fadae9eb9fd93b46c1c4cdaf16e.
    
    * [Config]: Revert config file to original state.
    
    * Update page.blade.php
    
    * Update LayoutHelper.php
    
    * Create iframe-base.blade.php
    
    * Add files via upload
    
    * Update adminlte.php
    
    * Update adminlte.php
    
    * Update adminlte.php
    
    Remove spaces on last implementation.
    
    * fix LayoutHelper
    
    Co-authored-by: Diego Smania <diego.smania@gmail.com>
    Co-authored-by: Florian Ressel <florian@genxtreme.de>

---
+++ resources/views/partials/common/content-base.blade.php
@@ -0,0 +1,19 @@
+{{-- Content Wrapper --}}
+<div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
+    {{-- Content Header --}}
+    @hasSection('content_header')
+        <div class="content-header">
+            <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
+                @yield('content_header')
+            </div>
+        </div>
+    @endif
+
+    {{-- Main Content --}}
+    <div class="content">
+        <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
+            @yield('content')
+        </div>
+    </div>
+</div>
+{{-- /.Content Wrapper--}}
