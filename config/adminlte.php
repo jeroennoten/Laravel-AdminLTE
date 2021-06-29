59df410c3e6a618447990ba4feda54fa86658f70
Warquia <warquia@gmail.com>
2021-06-29 12:59:39 -0300

    removing option
    Removing the "active: true/false" from the iframe as it will have a specific page for its activation

--- config/adminlte.php
+++ config/adminlte.php
@@ -89,8 +89,7 @@ return [
     'layout_fixed_navbar' => null,
     'layout_fixed_footer' => null,
     'layout_dark_mode' => null,
-    'layout_iframe' => [
-        'active' => false,
+    'layout_iframe' => [   
         'url-default' => [
             'url' => '',
             'title' => 'Dashboard',
