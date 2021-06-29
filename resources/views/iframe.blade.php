64028dc4c9e7f599c204ef705a840ab195fdbb27
Warquia <warquia@gmail.com>
2021-06-29 19:08:36 -0300

    Create iframe.blade.php (#938)
    * Create iframe.blade.php
    
    * Add extra line
    
    Co-authored-by: Diego Smania <shidersz@hotmail.com>

---
+++ resources/views/iframe.blade.php
@@ -0,0 +1,53 @@
+@extends('adminlte::master')
+
+@inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
+
+@if($layoutHelper->isLayoutTopnavEnabled())
+    @php( $def_container_class = 'container' )
+@else
+    @php( $def_container_class = 'container-fluid' )
+@endif
+
+@section('adminlte_css')
+    @stack('css')
+    @yield('css')
+@stop
+
+@section('classes_body', $layoutHelper->makeBodyClasses())
+
+@section('body_data', $layoutHelper->makeBodyData())
+
+@section('body')
+    <div class="wrapper">
+
+        {{-- Top Navbar --}}
+        @if($layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.navbar.navbar-layout-topnav')
+        @else
+            @include('adminlte::partials.navbar.navbar')
+        @endif
+
+        {{-- Left Main Sidebar --}}
+        @if(!$layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.sidebar.left-sidebar')
+        @endif
+
+        @include('adminlte::partials.iframe.iframe-base')
+
+        {{-- Footer --}}
+        @hasSection('footer')
+            @include('adminlte::partials.footer.footer')
+        @endif
+
+        {{-- Right Control Sidebar --}}
+        @if(config('adminlte.right_sidebar'))
+            @include('adminlte::partials.sidebar.right-sidebar')
+        @endif
+
+    </div>
+@stop
+
+@section('adminlte_js')
+    @stack('js')
+    @yield('js')
+@stop
