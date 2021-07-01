639fc5d82706ed8107f55606c6d6a27132798299
Diego Smania <diego.smania@gmail.com>
2021-06-30 23:46:14 -0300

    [Feature/IFrame]: Add base support to iframe mode feature.

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -33,25 +33,11 @@
         @endif
 
         {{-- Content Wrapper --}}
-        <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
-
-            {{-- Content Header --}}
-            @hasSection('content_header')
-                <div class="content-header">
-                    <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
-                        @yield('content_header')
-                    </div>
-                </div>
-            @endif
-
-            {{-- Main Content --}}
-            <div class="content">
-                <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
-                    @yield('content')
-                </div>
-            </div>
-
-        </div>
+        @empty($iframeEnabled)
+            @include('adminlte::partials.cwrapper.cwrapper-default')
+        @else
+            @include('adminlte::partials.cwrapper.cwrapper-iframe')
+        @endempty
 
         {{-- Footer --}}
         @hasSection('footer')
