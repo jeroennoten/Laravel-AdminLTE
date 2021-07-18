61952dcb6b8a3dddf8141b40683f33d86df2dac7
Diego Smania <diego.smania@gmail.com>
2021-07-18 13:54:17 -0300

    [resources]: Improve IFrame mode blade layout and configuration structure.

---
+++ resources/lang/en/iframe.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'Close',
+    'btn_close_active' => 'Close Active',
+    'btn_close_all' => 'Close All',
+    'btn_close_all_other' => 'Close Everything Else',
+    'tab_empty' => 'No tab selected!',
+    'tab_home' => 'Home',
+    'tab_loading' => 'Tab is loading',
+
+];
