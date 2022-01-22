d0a36de57116275692f463edf1b4dd9126387ffe
Diego Smania <diego.smania@gmail.com>
2022-01-22 18:54:48 -0300

    [Components] Add old support to date-range

--- src/View/Components/Form/DateRange.php
+++ src/View/Components/Form/DateRange.php
@@ -4,6 +4,8 @@ namespace JeroenNoten\LaravelAdminLte\View\Components\Form;
 
 class DateRange extends InputGroupComponent
 {
+    use Traits\OldValueSupportTrait;
+
     /**
      * The DateRangePicker plugin configuration parameters. Array with
      * key => value pairs, where the key should be an existing configuration
@@ -31,7 +33,8 @@ class DateRange extends InputGroupComponent
     public function __construct(
         $name, $id = null, $label = null, $igroupSize = null, $labelClass = null,
         $fgroupClass = null, $igroupClass = null, $disableFeedback = null,
-        $errorKey = null, $config = [], $enableDefaultRanges = null
+        $errorKey = null, $config = [], $enableDefaultRanges = null,
+        $enableOldSupport = null
     ) {
         parent::__construct(
             $name, $id, $label, $igroupSize, $labelClass, $fgroupClass,
@@ -40,6 +43,7 @@ class DateRange extends InputGroupComponent
 
         $this->config = is_array($config) ? $config : [];
         $this->enableDefaultRanges = $enableDefaultRanges;
+        $this->enableOldSupport = isset($enableOldSupport);
     }
 
     /**
