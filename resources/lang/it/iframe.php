035c468c5803938fcd6250e2f6a761e00c610e9d
Samuel <ratto_trash@hotmail.it>
2022-01-28 16:46:20 +0100

    Added Italian translation for iframe.php (#1047)

---
+++ resources/lang/it/iframe.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'Chiudi',
+    'btn_close_active' => 'Chiudi scheda',
+    'btn_close_all' => 'Chiudi tutto',
+    'btn_close_all_other' => 'Chiudi tutto il resto',
+    'tab_empty' => 'Nessuna scheda selezionata!',
+    'tab_home' => 'Home',
+    'tab_loading' => 'Caricamento scheda',
+];
