38bbd1d6a2ca9047051f37cc8d708ed712af3e37
Florian Ressel <florian@genxtreme.de>
2022-02-09 09:43:58 +0100

    add de lang file for iframe

---
+++ resources/lang/de/iframe.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'Schließen',
+    'btn_close_active' => 'Aktive schließen',
+    'btn_close_all' => 'Alle schließen',
+    'btn_close_all_other' => 'Alle anderen schließen',
+    'tab_empty' => 'Kein Tab ausgewählt!',
+    'tab_home' => 'Home',
+    'tab_loading' => 'Tab wird geladen',
+
+];
