1def5f979495ea964154128654937f392ce5b773
H.M Maruf <31335583+themaruf@users.noreply.github.com>
2022-06-28 03:50:07 +0600

    Add Bangla Language (bn) Translation (#1110)

---
+++ resources/lang/bn/iframe.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'বন্ধ করুন',
+    'btn_close_active' => 'একটিভ গুলো বন্ধ করুন',
+    'btn_close_all' => 'সব বন্ধ করুন',
+    'btn_close_all_other' => 'অন্য সবকিছু বন্ধ করুন',
+    'tab_empty' => 'কোন ট্যাব নির্বাচন করা হয়নি!',
+    'tab_home' => 'হোম',
+    'tab_loading' => 'ট্যাব লোড হচ্ছে',
+
+];
---
+++ resources/lang/bn/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'প্রধান নেভিগেশান',
+    'blog'                          => 'ব্লগ',
+    'pages'                         => 'পেজ',
+    'account_settings'              => 'অ্যাকাউন্ট সেটিংস',
+    'profile'                       => 'প্রোফাইল',
+    'change_password'               => 'পাসওয়ার্ড পরিবর্তন করুন',
+    'multilevel'                    => 'মাল্টি লেভেল',
+    'level_one'                     => 'লেভেল ১',
+    'level_two'                     => 'লেভেল ২',
+    'level_three'                   => 'লেভেল ৩',
+    'labels'                        => 'লেবেল',
+    'important'                     => 'গুরুত্বপূর্ণ',
+    'warning'                       => 'সতর্কতা',
+    'information'                   => 'তথ্য',
+];
