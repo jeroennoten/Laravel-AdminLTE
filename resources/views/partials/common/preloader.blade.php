d64b0f8d482eda9266e77cb7568fe360dfbcf118
KostKostY1 <kostikas420@gmail.com>
2022-06-29 20:28:40 +0300

    Fixed StyleCI Errors

--- resources/views/partials/common/preloader.blade.php
+++ resources/views/partials/common/preloader.blade.php
@@ -3,7 +3,7 @@
 <div class="preloader flex-column justify-content-center align-items-center">
 
     <img class="animation__shake" src="{{ asset(config('adminlte.preloader_image', 'vendor/adminlte/dist/img/AdminLTELogo.png')) }}"
-         alt="{{ config('adminlte.preloader_alt', 'AdminLTE')}}"
+         alt="{{  config('adminlte.preloader_alt', 'AdminLTE')}}"
          width="{{ config('adminlte.preloader_img_width', '60')}}"
          height="{{ config('adminlte.preloader_img_height', '60')}}" >
 </div>
