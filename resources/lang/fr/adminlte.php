0f7717d58a95e84f002f94dae1f9c332a10307c8
billyboy35 <kevin.niglaut@wanadoo.fr>
2022-11-25 21:13:17 +0100

    added french translation

--- resources/lang/fr/adminlte.php
+++ resources/lang/fr/adminlte.php
@@ -19,4 +19,11 @@ return [
     'password_reset_message'      => 'Réinitialisation du mot de passe',
     'reset_password'              => 'Réinitialisation du mot de passe',
     'send_password_reset_link'    => 'Envoi de la réinitialisation du mot de passe',
+    'verify_message'              => 'Votre compte a besoin d\'une vérification',
+    'verify_email_sent'           => 'Un nouveau lien de vérification a été envoyé à votre adresse e-mail.',
+    'verify_check_your_email'     => 'Avant de continuer, veuillez vérifier votre e-mail pour un lien de vérification.',
+    'verify_if_not_recieved'      => 'Si vous n\'avez pas reçu l\'e-mail',
+    'verify_request_another'      => 'cliquez ici pour en demander un autre',
+    'confirm_password_message'    => 'Veuillez confirmer votre mot de passe pour continuer.',
+    'remember_me_hint'            => 'Gardez-moi authentifié indéfiniment ou jusqu\'à ce que je me déconnecte manuellement',
 ];
