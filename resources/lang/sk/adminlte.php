17da5e5a4b979baf3a101cde8895a7ee9da272fd
Stefan Lucivjansky <stefan.lucivjansky@synpulse.com>
2022-12-04 23:24:07 +0100

    Added Slovak language translation

---
+++ resources/lang/sk/adminlte.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Celé meno',
+    'email'                       => 'Email',
+    'password'                    => 'Heslo',
+    'retype_password'             => 'Zopakujte heslo',
+    'remember_me'                 => 'Zapamätať si ma',
+    'register'                    => 'Registrovať',
+    'register_a_new_membership'   => 'Registrovať nový účet',
+    'i_forgot_my_password'        => 'Zabudol som heslo',
+    'i_already_have_a_membership' => 'Už mám účet',
+    'sign_in'                     => 'Prihlásiť',
+    'log_out'                     => 'Odhlásiť',
+    'toggle_navigation'           => 'Prepnúť navigáciu',
+    'login_message'               => 'Pre pokračovanie sa prihláste',
+    'register_message'            => 'Registrovať nový účet',
+    'password_reset_message'      => 'Obnoviť heslo',
+    'reset_password'              => 'Obnoviť heslo',
+    'send_password_reset_link'    => 'Zaslať odkaz na obnovenie hesla',
+    'verify_message'              => 'Váš účet je potrebné overiť',
+    'verify_email_sent'           => 'Na vašu emailovú adresu bol odoslaný nový odkaz na overenie účtu.',
+    'verify_check_your_email'     => 'Pred tým, než budete pokračovať, skontrolujte svoju emailovú adresu pre overovací odkaz.',
+    'verify_if_not_recieved'      => 'V prípade, že ste email neobdržali',
+    'verify_request_another'      => 'kliknite sem pre obdržanie ďalšieho',
+    'confirm_password_message'    => 'Pre pokračovanie prosím potvrďte svoje heslo.',
+    'remember_me_hint'            => 'Udržiavať prihlásenie bez obmedzenia, alebo kým sa neodhlásim',
+];
