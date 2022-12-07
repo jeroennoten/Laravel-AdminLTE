8d5f3fc4e8041039a2bdf640e10457fd5016258a
billyboy35 <75578469+billyboy35@users.noreply.github.com>
2022-12-07 20:04:01 +0100

    added french translation (#1147)

---
+++ resources/lang/fr/iframe.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'Fermer',
+    'btn_close_active' => 'Fermer Actif',
+    'btn_close_all' => 'Tout fermer',
+    'btn_close_all_other' => 'Fermer tout le reste',
+    'tab_empty' => 'Aucun onglet sélectionné !',
+    'tab_home' => 'Accueil',
+    'tab_loading' => 'Onglet en cours de chargement',
+
+];
---
+++ resources/lang/fr/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MENU PRINCIPALE',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Pages',
+    'account_settings'              => 'PARAMÈTRES DU COMPTE',
+    'profile'                       => 'Profil',
+    'change_password'               => 'Change Password',
+    'multilevel'                    => 'Multi niveau',
+    'level_one'                     => 'Niveau 1',
+    'level_two'                     => 'Niveau 2',
+    'level_three'                   => 'Niveau 3',
+    'labels'                        => 'LABELS',
+    'important'                     => 'Important',
+    'warning'                       => 'Avertissement',
+    'information'                   => 'Informations',
+];
