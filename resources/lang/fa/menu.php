4ba6834c6ba86c9cec35427d2ef49108fae634e5
Amin Esmkhani <amin.esmkhani75@gmail.com>
2023-05-09 22:04:06 +0430

    Create fa/menu.php & Translate To Persian!

---
+++ resources/lang/fa/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'ناو بار اصلی',
+    'blog'                          => 'بلاگ',
+    'pages'                         => 'صفحات',
+    'account_settings'              => 'تنظیمات اکانت',
+    'profile'                       => 'پروفایل',
+    'change_password'               => 'تغییر رمز عبور',
+    'multilevel'                    => 'چند سطحی',
+    'level_one'                     => 'سطح ۱',
+    'level_two'                     => 'سطح ۲',
+    'level_three'                   => 'سطح ۳',
+    'labels'                        => 'برچسب ها',
+    'important'                     => 'مهم',
+    'warning'                       => 'هشدار',
+    'information'                   => 'اطلاعات',
+];
