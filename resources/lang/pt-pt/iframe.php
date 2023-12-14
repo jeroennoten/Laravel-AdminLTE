038a322fb0e77b2ec88a11fa7e5061d172a5c5f8
Ricardo Mestre <ricardo.mestre@suse.com>
2023-12-14 14:39:57 +0000

    add pt-pt translations

---
+++ resources/lang/pt-pt/iframe.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IFrame Mode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the AdminLTE IFrame mode blade
+    | layout. You are free to modify these language lines according to your
+    | application's requirements.
+    |
+    */
+
+    'btn_close' => 'Fechar',
+    'btn_close_active' => 'Fechar Ativo',
+    'btn_close_all' => 'Fechar Todos',
+    'btn_close_all_other' => 'Fechar Outros',
+    'tab_empty' => 'Nenhum separador selecionado!',
+    'tab_home' => 'Página Inicial',
+    'tab_loading' => 'A carregar separador',
+
+];
