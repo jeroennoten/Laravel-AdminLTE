a7d16fc2190e32d51f428ff6a9589de949fa50e7
Ricardo Mestre <ricardo.mestre@suse.com>
2023-12-19 18:26:27 +0000

    add pt-pt translations (#1236)

---
+++ resources/lang/pt-pt/adminlte.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nome completo',
+    'email'                       => 'Email',
+    'password'                    => 'Palavra-passe',
+    'retype_password'             => 'Repita a palavra-passe',
+    'remember_me'                 => 'Lembrar-me',
+    'register'                    => 'Registar',
+    'register_a_new_membership'   => 'Registar um novo membro',
+    'i_forgot_my_password'        => 'Esqueci-me da palavra-passe',
+    'i_already_have_a_membership' => 'Já sou membro',
+    'sign_in'                     => 'Entrar',
+    'log_out'                     => 'Sair',
+    'toggle_navigation'           => 'Alternar navegação',
+    'login_message'               => 'Entre para iniciar nova sessão',
+    'register_message'            => 'Registar um novo membro',
+    'password_reset_message'      => 'Recuperar palavra-passe',
+    'reset_password'              => 'Recuperar palavra-passe',
+    'send_password_reset_link'    => 'Enviar link de recuperação de palavra-passe',
+    'verify_message'              => 'A sua conta precisa ser verificada',
+    'verify_email_sent'           => 'Um novo link de verificação foi enviado para o seu email.',
+    'verify_check_your_email'     => 'Antes de continuar, por favor verifique o seu email com o link de confirmação.',
+    'verify_if_not_recieved'      => 'caso não tenha recebido o email',
+    'verify_request_another'      => 'clique aqui para solicitar um novo',
+    'confirm_password_message'    => 'Por favor, confirme a sua palavra-passe para continuar.',
+];
